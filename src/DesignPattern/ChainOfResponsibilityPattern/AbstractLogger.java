6637e35c70320d7b6acfac7a018111a7d77472a4
Solomon Ge <gesuheng@163.com>
2015-05-07 19:00:53 +0800

    ProxyImageChange

---
+++ src/DesignPattern/ChainOfResponsibilityPattern/AbstractLogger.java
@@ -0,0 +1,15 @@
+package DesignPattern.ChainOfResponsibilityPattern;
+
+/**
+ * Created by Suheng on 15/5/7.
+ */
+public abstract class AbstractLogger {
+    public static int INFO = 1;
+    public static int DEBUG = 2;
+    public static int ERROR = 3;
+
+    protected int level;
+
+    //next element in chain or responsibility
+    protected AbstractLogger
+}
--- src/DesignPattern/ProxyPattern/Image.java
+++ src/DesignPattern/ProxyPattern/Image.java
@@ -1,4 +1,4 @@
-package DesignPattern;
+package DesignPattern.ProxyPattern;
 
 /**
  * Created by Suheng on 15/5/7.
